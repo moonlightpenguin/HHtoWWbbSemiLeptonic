a684db5754aa5c0d2c384b26bd54116c696f8b32
Mathis Frahm <mathisfrahm@gmx.de>
2021-06-03 10:21:08 +0200

    changed structure of NNApplication

--- include/HHtoWWbbSemiLeptonicMulticlassNNHists.h
+++ include/HHtoWWbbSemiLeptonicMulticlassNNHists.h
@@ -12,6 +12,7 @@ public:
   void init();
 
   TH1F *NN_out0, *NN_out1, *NN_out2, *NN_out3, *NN_out4;
+  TH1F *NN_out0_rebin, *NN_out1_rebin, *NN_out2_rebin, *NN_out3_rebin, *NN_out4_rebin;
   uhh2::Event::Handle<double> h_NNoutput0;
   uhh2::Event::Handle<double> h_NNoutput1;
   uhh2::Event::Handle<double> h_NNoutput2;
@@ -34,7 +35,7 @@ public:
   //high-level
   uhh2::Event::Handle<float> h_mbb, h_mlnu, h_mqq, h_DeltaRlnu, h_DeltaRbb, h_DeltaRqq, h_minDeltaRlj, h_minDeltaRbj, h_minDeltaRjj, h_HT, h_N_BTag, h_N_Ak4, h_mtop_lep_hyp1, h_mtop_lep_hyp2, h_mtop_had_hyp1, h_mtop_had_hyp2, h_MH_bb, h_MH_WW;
 
-  TH1F *NN_mbb, *NN_mlnu, *NN_mqq, *NN_DeltaRlnu, *NN_DeltaRbb, *NN_DeltaRqq, *NN_minDeltaRlj, *NN_minDeltaRbj, *NN_minDeltaRjj, *NN_HT, *NN_NBTag, *NN_NAk4, *NN_mtop_lep1, *NN_mtop_lep2, *NN_mtop_had1, *NN_mtop_had2;
+  TH1F *NN_mbb, *NN_mlnu, *NN_mqq, *NN_DeltaRlnu, *NN_DeltaRbb, *NN_DeltaRqq, *NN_minDeltaRlj, *NN_minDeltaRbj, *NN_minDeltaRjj, *NN_HT, *NN_NBTag, *NN_NAk4, *NN_mtop_lep1, *NN_mtop_lep2, *NN_mtop_had1, *NN_mtop_had2, *NN_MH_bb, *NN_MH_WW;
 
   //low-level
   uhh2::Event::Handle< float > h_Lep_pt, h_Lep_eta, h_Lep_phi, h_Lep_E;
